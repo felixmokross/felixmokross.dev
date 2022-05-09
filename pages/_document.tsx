b253a88f6e49cb91da179629c6bb945454753ab2
Felix Mokross <felixmokross@hey.com>
2022-05-09 11:04:48 +0200

    Remove smooth scrolling (messes up page transitions, there it should not scroll like this)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import { alternateSiteTitle, baseUrl, domain } from "../src/util";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" className="scroll-smooth">
+      <Html lang="en">
         <Head>
           <link
             rel="alternate"
