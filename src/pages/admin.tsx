05d07d24a9e9c8cdc9c76b140f89d856d3ccbbad
Felix Mokross <felixmokross@hey.com>
2022-05-20 16:10:59 +0200

    Add button to enable/disable plausible_ignore setting

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getBranchesFromGithub } from "../shared/github.server";
 import { Layout } from "../shared/layout";
 import {
@@ -10,9 +10,19 @@ import {
   PreviewData,
 } from "../shared/util.server";
 import { getSession, signOut } from "next-auth/react";
+import { Switch } from "@headlessui/react";
+import { cn } from "../shared/classnames";
 
 export default function AdminPage({ branches, layoutProps }: AdminPageProps) {
   const [branch, setBranch] = useState(branches[0]);
+  const [excludedFromAnalytics, setExcludedFromAnalytics] = useState(false);
+
+  useEffect(() => {
+    setExcludedFromAnalytics(
+      localStorage.getItem("plausible_ignore") === "true"
+    );
+  }, []);
+
   const router = useRouter();
 
   return (
@@ -81,6 +91,32 @@ export default function AdminPage({ branches, layoutProps }: AdminPageProps) {
           >
             Sign out
           </button>
+
+          <Switch
+            checked={excludedFromAnalytics}
+            onChange={() => {
+              if (excludedFromAnalytics) {
+                localStorage.removeItem("plausible_ignore");
+                setExcludedFromAnalytics(false);
+              } else {
+                localStorage.setItem("plausible_ignore", "true");
+                setExcludedFromAnalytics(true);
+              }
+            }}
+            className={cn(
+              excludedFromAnalytics ? "bg-sky-600" : "bg-gray-200",
+              "relative mt-4 inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
+            )}
+          >
+            <span className="sr-only">Use setting</span>
+            <span
+              aria-hidden="true"
+              className={cn(
+                excludedFromAnalytics ? "translate-x-5" : "translate-x-0",
+                "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+              )}
+            />
+          </Switch>
         </div>
       </div>
     </Layout>
