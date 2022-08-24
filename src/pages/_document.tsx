ddfb6735c228287e77c5e44f0432415b3e77412f
Felix Mokross <felixmokross@gmail.com>
2022-08-24 22:36:40 +0200

    Improve favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,12 +9,38 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png?v=3"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png?v=3"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png?v=3"
+          />
+          <link rel="manifest" href="/site.webmanifest?v=3" />
+          <link
+            rel="mask-icon"
+            href="/safari-pinned-tab.svg?v=3"
+            color="#5bbad5"
+          />
+          <link rel="shortcut icon" href="/favicon.ico?v=3" />
+          <meta name="msapplication-TileColor" content="#00aba9" />
+          <meta name="theme-color" content="#ffffff" />
+
+          <link
             rel="alternate"
             type="application/rss+xml"
             title={`RSS Feed for ${alternateSiteTitle}`}
             href={rssUrl}
           />
-          <link rel="icon" type="image/png" href="/favicon_v2.png" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=optional"
             rel="stylesheet"
