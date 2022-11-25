b5d632712f712e6994a89fe664a5cdb0d1373876
Felix Mokross <felixmokross@gmail.com>
2022-11-25 22:46:22 +0100

    Migrate home page to server component (straight forward for this page)

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,17 @@
 import { getAllPosts } from "../../src/shared/posts.server";
-import { HomePage } from "./home-page";
 import { previewData } from "next/headers";
+import { HomeContainer } from "../../src/blog/index/home-container";
+import { Bio } from "../../src/blog/index/bio";
+import { PostList } from "../../src/blog/index/post-list";
 
 export default async function Page() {
   const preview = previewData();
   const posts = await getAllPosts(preview.branch);
 
-  return <HomePage posts={posts} />;
+  return (
+    <HomeContainer>
+      <Bio />
+      <PostList posts={posts} />
+    </HomeContainer>
+  );
 }
