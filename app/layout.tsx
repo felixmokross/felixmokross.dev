d2312d1cf1db5144bad20642299e9f185551be85
Felix Mokross <felixmokross@gmail.com>
2022-11-27 14:21:25 +0100

    Add viewport meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default async function RootLayout({ children }: PropsWithChildren<{}>) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
